58d852aa64a0f892a0230f2e9444fba562e9192c
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-01-12 08:51:36 +0200

    scaled CNF and DNF generators

--- tests/CNF.hs
+++ tests/CNF.hs
@@ -7,14 +7,18 @@ import LatticeLaws
 
 import Test.Tasty
 import Test.QuickCheck
+import Test.QuickCheck.Gen
 
 import Control.Applicative
 import Data.Function (on)
 
 import Algebra.Boolean.NormalForms
 
+scaled :: (Int -> Int) -> Gen a -> Gen a
+scaled f (MkGen m) = MkGen (\r n -> m r (f n))
+
 instance  Arbitrary a => Arbitrary (CNF a) where
-  arbitrary = lowerFreeLattice . (fmap liftCNF) <$> arbitrary
+  arbitrary = lowerFreeLattice . (fmap liftCNF) <$> scaled (min 10) arbitrary
 
 tests :: TestTree
 tests = testGroup "CNF list implementation"
--- tests/DNF.hs
+++ tests/DNF.hs
@@ -7,14 +7,18 @@ import LatticeLaws
 
 import Test.Tasty
 import Test.QuickCheck
+import Test.QuickCheck.Gen
 
 import Control.Applicative
 import Data.Function (on)
 
 import Algebra.Boolean.NormalForms
 
+scaled :: (Int -> Int) -> Gen a -> Gen a
+scaled f (MkGen m) = MkGen (\r n -> m r (f n))
+
 instance  Arbitrary a => Arbitrary (DNF a) where
-  arbitrary = lowerFreeLattice . (fmap liftDNF) <$> arbitrary
+  arbitrary = lowerFreeLattice . (fmap liftDNF) <$> scaled (min 10) arbitrary
 
 tests :: TestTree
 tests = testGroup "DNF list implementation"
